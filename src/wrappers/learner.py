e8fa235a652ad572adb4b51d8c9feda6020f46a3
labombatomba <t.gallien@gmx.at>
2024-12-15 20:18:17 +0100

    optuna hparams search support added

--- src/wrappers/learner.py
+++ src/wrappers/learner.py
@@ -1,8 +1,11 @@
 from typing import Any, Optional, List
-
+from stable_baselines3.common.evaluation import evaluate_policy
         
 def sb3_learner(agent, callback: Optional[List]= None, **kwargs) -> Any: 
     if callback:
         callback = list(callback)
-    return agent.learn(callback=callback, **kwargs)
-
+       
+    agent.learn(callback=callback, **kwargs)
+    mean_reward, _ = evaluate_policy(model=agent, env=agent.get_env())
+    
+    return mean_reward
